a4df77aae5db2aa0bc7a5f7d88670145434aa327
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2021-01-18 12:00:15 -0500

    update unit aliases

--- miyu_bot/commands/common/name_aliases.py
+++ miyu_bot/commands/common/name_aliases.py
@@ -12,21 +12,27 @@ units_by_name['rondo'] = units_by_name['燐舞曲']
 units_by_name['special'] = units_by_name['スペシャル']
 units_by_name['other'] = units_by_name['その他']
 unit_aliases = {
+    'happyaround': 'happy_around',
     'hapiara': 'happy_around',
     'ha': 'happy_around',
+    'peakyp-key': 'peaky_p-key',
+    'peakypkey': 'peaky_p-key',
     'peaky': 'peaky_p-key',
     'p-key': 'peaky_p-key',
     'pkey': 'peaky_p-key',
     'pkpk': 'peaky_p-key',
     'pk': 'peaky_p-key',
+    'photonmaiden': 'photon_maiden',
     'photome': 'photon_maiden',
     'photon': 'photon_maiden',
     'pm': 'photon_maiden',
     'mermaid': 'merm4id',
     'mmd': 'merm4id',
+    'lyricallily': 'lyrical_lily',
     'riririri': 'lyrical_lily',
     'lililili': 'lyrical_lily',
     'lily': 'lyrical_lily',
     'lili': 'lyrical_lily',
     'll': 'lyrical_lily',
+    'fuhifumi': 'lyrical_lily',
 }

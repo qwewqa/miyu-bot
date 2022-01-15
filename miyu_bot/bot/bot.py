578535c7c98cf8ed2ed5ff494f5bad7b363aa3e0
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2022-01-15 14:43:02 -0800

    Switch assets url

--- miyu_bot/bot/bot.py
+++ miyu_bot/bot/bot.py
@@ -31,7 +31,7 @@ class MiyuBot(commands.Bot):
     thread_pool: ThreadPoolExecutor
 
     asset_path: Path
-    asset_url = 'https://qwewqa.github.io/miyu-assets/'
+    asset_url = 'https://qwewqa.github.io/miyu-data/'
 
     config: dict
     scripts_path: Optional[Path]

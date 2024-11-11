7d665333d33dd5dc8651c2bb8d5454c08b6d93e4
Mirby <mirbyc123@gmail.com>
2024-11-11 13:41:21 -0500

    expand stamp embed with image

--- miyu_bot/commands/master_filter/stamp_filter.py
+++ miyu_bot/commands/master_filter/stamp_filter.py
@@ -1,5 +1,7 @@
 import discord
 from d4dj_utils.master.stamp_master import StampMaster
+from miyu_bot.commands.common.asset_paths import get_asset_filename
+from typing import Tuple
 
 from miyu_bot.commands.master_filter.master_filter import (
     MasterFilter,
@@ -14,10 +16,18 @@ class StampFilter(MasterFilter[StampMaster]):
             return value.name
         else:
             return f'{value.name + " " + value.quote.replace("～", "ー") if value.quote else value.description}'
+        
+    def get_select_name(self, value: StampMaster) -> Tuple[str, str, None]:
+        return f"{value.name}", f'{value.quote.replace("～", "ー") if value.quote else value.description}', None
 
     @command_source()
     def get_stamp_embed(self, ctx, stamp: StampMaster, server):
+        l10n = self.l10n[ctx]
         embed = discord.Embed(title=self.format_stamp_name(stamp), description="ID: " + str(stamp.id))
+
+        embed.set_image(url=self.bot.asset_url + get_asset_filename(stamp.stamp_path))
+
+        embed.set_footer(text=l10n.format_value("stamp-id", {"stamp-id": f"{stamp.id:>07}"}))
         return embed
 
     @list_formatter(
